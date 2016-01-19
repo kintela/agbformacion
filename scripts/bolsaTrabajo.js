b48c9e254a07c63a3da39d497337fa65077b9b9b
kintela <kintela@innovup.cc>
2016-01-19 19:44:41 +0100

    pruebas envio mail

--- scripts/bolsaTrabajo.js
+++ scripts/bolsaTrabajo.js
@@ -17,4 +17,23 @@ function handleFileSelect(evt) {
     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
  }
   
+ var mandrill = require('node-mandrill')('ohEjhPgs1DjrfDCwfRYliQ'); 
+
+function sendMail ( _name, _email, _subject, _message) {
+    mandrill('/messages/send', {
+        message: {
+            to: [{email: _email , name: _name}],
+            from_email: 'info@innovup.cc',
+            subject: _subject,
+            text: _message
+        }
+    }, function(error, response){
+        if (error) console.log( error );
+        else console.log(response);
+    });
+}
+
+function enviarMail(){
+    alert("Hola");
+}
   
