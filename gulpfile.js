d9bf0de8173e5c98deb97b1f3483a47681347008
kintela <kintela@innovup.cc>
2016-01-20 16:37:42 +0100

    Separar tarea de creación de servidor wen "gulp-serve" y configuración del fichero tasks.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,1 @@
-var gulp = require('gulp');
-var serve = require('gulp-serve');
-
-gulp.task('serve', serve('wwwroot'));
-
-gulp.task('serve-build', serve(['wwwroot', 'build']));
-
-gulp.task('serve-prod', serve({
-  root: ['wwwroot', 'build'],
-  port: 80,
-  middleware: function(req, res) {
-    // custom optional middleware 
-  }
-}));
-
 require('require-dir')('gulp-build/tasks');
