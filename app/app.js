456b60e8e32ef28be5ebe4e00f683198cbdfa05d
kintela <ROBERTO.QUINTELA@GMAIL.COM>
2016-02-17 23:01:11 +0100

    terminar enlaces de "Que ofrecemos"

--- app/app.js
+++ app/app.js
@@ -8,40 +8,68 @@ agbApp.config(['$routeProvider', function ($routeProvider) {
         }) 
         .when('/risoterapia', {
             templateUrl: 'app/components/risoterapia/risoterapia.html',
-            title: 'Inicio'
-        })    
+            title: 'risoterapia'
+        })   
+        .when('/risoterapia-visor-fotos', {
+            templateUrl: 'app/components/risoterapia/visor-fotos.html',
+            title: 'risoterapia-visor-fotos'
+        })             
          .when('/que-ofrecemos', {
             templateUrl: 'app/components/que-ofrecemos/que-ofrecemos.html',
-            title: 'Inicio'
+            title: 'que-ofrecemos'
         })   
         .when('/formacion-continua-privada', {
             templateUrl: 'app/components/que-ofrecemos/formacion-continua-privada.html',
-            title: 'Inicio'
+            title: 'formacion-continua-privada'
         })  
          .when('/formacion-continua-subvencionada', {
             templateUrl: 'app/components/que-ofrecemos/formacion-continua-subvencionada.html',
-            title: 'Inicio'
+            title: 'formacion-continua-subvencionada'
+        })   
+         .when('/fondo-social-europeo', {
+            templateUrl: 'app/components/que-ofrecemos/fondo-social-europeo.html',
+            title: 'fondo-social-europeo'
+        })    
+        .when('/hobetuz', {
+            templateUrl: 'app/components/que-ofrecemos/hobetuz.html',
+            title: 'hobetuz'
+        })     
+        .when('/nuevo-modelo', {
+            templateUrl: 'app/components/que-ofrecemos/nuevo-modelo.html',
+            title: 'nuevo-modelo'
         })    
         .when('/clientes', {
             templateUrl: 'app/components/clientes/clientes.html',
-            title: 'Inicio'
+            title: 'clientes'
         })       
         .when('/equipo', {
-                    templateUrl: 'app/components/equipo/equipo.html',
-                    title: 'Inicio'
-                }) 
+            templateUrl: 'app/components/equipo/equipo.html',
+            title: 'equipo'
+        }) 
         .when('/alianzas', {
-                    templateUrl: 'app/components/alianzas/alianzas.html',
-                    title: 'Inicio'
-                }) 
+            templateUrl: 'app/components/alianzas/alianzas.html',
+            title: 'alianzas'
+        }) 
         .when('/bolsa-trabajo', {
-                    templateUrl: 'app/components/bolsa-trabajo/bolsa-trabajo.html',
-                    title: 'Inicio'
-                }) 
+            templateUrl: 'app/components/bolsa-trabajo/bolsa-trabajo.html',
+            title: 'bolsa-trabajo'
+        }) 
         .when('/contacto', {
-                    templateUrl: 'app/components/contacto/contacto.html',
-                    title: 'Inicio'
-                }) 
+            templateUrl: 'app/components/contacto/contacto.html',
+            title: 'contacto'
+        }) 
+        .when('/outdoor-training', {
+            templateUrl: 'app/components/outdoor-training/outdoor-training.html',
+            title: 'outdoor-training'
+        }) 
+        .when('/outdoor-visor-fotos', {
+            templateUrl: 'app/components/outdoor-training/visor-fotos.html',
+            title: 'outdoor-visor-fotos'
+        }) 
+        .when('/crecimiento-personal', {
+            templateUrl: 'app/components/crecimiento-personal/crecimiento-personal.html',
+            title: 'crecimiento-personal'
+        }) 
 
         .otherwise({
             redirectTo: '/'
