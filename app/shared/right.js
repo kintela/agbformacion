590b5c5eaa586c7bc5eface85d76833603e5c413
kintela <ROBERTO.QUINTELA@GMAIL.COM>
2016-02-17 23:40:39 +0100

    Desplegables zona derecha

--- app/shared/right.js
+++ app/shared/right.js
@@ -1,8 +1,31 @@
 angular.module('right', [])        
        .controller('rightController', ['$scope', '$location', function ($scope, $location) {
 
-           $scope.mostrarRisoterapia = function (project) {
+           $scope.mostrarRisoterapiaOLD = function (project) {
                //$location.path('/proyecto/' + project.node.Codigo + '/' + project.node.Descripcion);
                alert("Hola");
            };
+           
+           $scope.mostrarRisoterapia=function mostrarRisoterapia(){
+                var elemento=document.getElementById('subMenusRisoterapia');
+
+                if(elemento.style.display=="none"){
+                    document.getElementById('subMenusRisoterapia').style.display = 'block';
+                }
+                else{
+                    document.getElementById('subMenusRisoterapia').style.display = 'none';
+                }
+            };
+            
+            $scope.mostrarOutdoor=function mostrarOutdoor(){
+                var elemento=document.getElementById('subMenusOutdoor');
+
+                if(elemento.style.display=="none"){
+                    document.getElementById('subMenusOutdoor').style.display = 'block';
+                }
+                else{
+                    document.getElementById('subMenusOutdoor').style.display = 'none';
+                }
+            };
+
         }]);
