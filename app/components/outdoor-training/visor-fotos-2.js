02ba40125a91e5c932f242c6e174360de8b43993
kintela <kintela@innovup.cc>
2016-03-16 10:30:08 +0100

    Añadir fotos

--- app/components/outdoor-training/visor-fotos-2.js
+++ app/components/outdoor-training/visor-fotos-2.js
@@ -25,5 +25,121 @@ angular.module('carouselOutdoor', ['ui.bootstrap'])
             id:currIndex++
         });
 
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-bandera.jpg',
+            text:'Bandera',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-calentamiento.jpg',
+            text:'Calentamiento',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-casa-confianza.jpg',
+            text:'Confianza',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-esquis.jpg',
+            text:'Esquís',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-meteoritos.jpg',
+            text:'Meteoritos',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-puente.jpg',
+            text:'Puente',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-rio-acido.jpg',
+            text:'Rio Acido',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-sacos-de-gulliver.jpg',
+            text:'Sacos de Gulliver',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-telarana.jpg',
+            text:'Telaraña',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-travesia-lago.jpg',
+            text:'Travesía del lago',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-aula.jpg',
+            text:'Aula',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-catapultas.jpg',
+            text:'Catapulta',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-equipo-rescata.jpg',
+            text:'Equipo de rescate',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/agb-outdoor-training-Bilbao-Vitoria-Donosti-Pais-Vasco-puente-humano.jpg',
+            text:'Puente humano',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/campo-de-minas.jpg',
+            text:'Campo de Minas',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/precision-multiple.jpg',
+            text:'Precisión múltiple',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/Outdoor-Team-Building-Bilbao-Vitoria-Donosti-Poligono-Ciego.jpg',
+            text:'Polígono Ciego',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/Outdoor-Team-Building-Bilbao-Vitoria-Donosti-Puzzle-Loco.jpg',
+            text:'Puzzle Loco',
+            id:currIndex++
+        });
+
+        slides.push({
+            image: 'img/fotos/outdoor-training/Outdoor--Team-Building-Bilbao-Vitoria-Donosti-Telarana-Vertical.jpg',
+            text:'Telaraña vertical',
+            id:currIndex++
+        });
+
+
+
 
         }]);
